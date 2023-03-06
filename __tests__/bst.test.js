0f32239dc205ccbc012e41fbdd249b3ba498a7d6
Samuel Majerus <ladolego@gmail.com>
2023-03-06 11:41:37 -0800

    Pass -- BST Search logic, 1st test. (Lsn 10 [see link])
    https://www.learnhowtoprogram.com/computer-science/big-o-notation-and-binary-trees/binary-search-trees-part-3

--- __tests__/bst.test.js
+++ __tests__/bst.test.js
@@ -74,4 +74,23 @@ describe('binarySearchTree', () => {
     let node2 = new BSTNode(36);
     expect(bst.insertNode(node2)).toEqual({"root": {"data": 36, "left": null, "right": null}});
   });
+
+
+
+  //Tests for Search prototype (Lsn 10) 
+  const pbst = new BST();
+
+  beforeEach(() => {
+    pbst.insertNode(new BSTNode(4));
+    pbst.insertNode(new BSTNode(2));
+    pbst.insertNode(new BSTNode(6));
+    pbst.insertNode(new BSTNode(1));
+    pbst.insertNode(new BSTNode(3));
+    pbst.insertNode(new BSTNode(5));
+    pbst.insertNode(new BSTNode(7));  
+  });
+
+  test('it should return true if the root node is equal to 4', () => {
+    expect(pbst.search(4)).toEqual(true);
+  });
 });
